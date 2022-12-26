2ee057a1995c35b636b174d81c2532f82beace4d
Odorico Veloso <odoricoveloso@gmail.com>
2022-12-25 22:31:39 -0300

    Change to work on firefox with webdriver-manager

--- captions.py
+++ captions.py
@@ -1,9 +1,9 @@
 # modify these values
-filename = 'videolist_zembla_273_2018_05_25-09_17_02.tab'			# filname with video ids
+filename = 'coronavirus.csv'										# filname with video ids
 colname = 'videoId'													# column storing video ids
-delimiter = '\t'													# delimiter, e.g. ',' for CSV or '\t' for TAB
-waittime = 10														# seconds browser waits before giving up
-sleeptime = [5,15]													# random seconds range before loading next video id
+delimiter = ','														# delimiter, e.g. ',' for CSV or '\t' for TAB
+waittime = 7														# seconds browser waits before giving up
+sleeptime = [5,10]													# random seconds range before loading next video id
 headless = True														# select True if you want the browser window to be invisible (but not inaudible)
 
 #do not modify below
@@ -11,13 +11,24 @@ from time import sleep
 import csv
 import random
 import os.path
+from datetime import datetime
+import pytz
+import logging
 from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.firefox.options import Options
 
+# logging config
+logging.basicConfig(filename = 'captions.log', filemode = 'w', level = logging.INFO, format = '%(message)s', force = True)
+
+# datetime config
+datetimesp = datetime.now(pytz.timezone('America/Sao_Paulo'))
+dt = datetimesp.strftime('%d/%m/%Y %H:%M')
 
 def gettranscript(videoid):
 
@@ -31,10 +42,15 @@ def gettranscript(videoid):
 
 	options = Options()
 	options.add_argument("--headless")
+	options.add_argument('--no-sandbox')
+	options.add_argument('--disable-dev-shm-usage')
+	#options.binary_location = r'C:/Program Files/Mozilla Firefox/firefox.exe'
+	#service = FirefoxService('geckodriver.exe')
 
 	# Create a new instance of the Firefox driver
 	if headless:
-		driver = webdriver.Firefox(firefox_options=options)
+		#driver = webdriver.Firefox(options=options, service=service)
+		driver = webdriver.Firefox(options=options, service=FirefoxService(GeckoDriverManager().install()))
 	else:
 		driver = webdriver.Firefox()
 
@@ -42,7 +58,7 @@ def gettranscript(videoid):
 	driver.get("https://www.youtube.com/watch?v="+videoid)
 
 	try:
-	    element = WebDriverWait(driver, waittime).until(EC.presence_of_element_located((By.CSS_SELECTOR, "yt-icon-button.dropdown-trigger > button:nth-child(1)")))
+		element = WebDriverWait(driver, waittime).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'ytd-menu-renderer.ytd-watch-metadata > yt-button-shape:nth-child(4) > button:nth-child(1) > yt-touch-feedback-shape:nth-child(2) > div:nth-child(1) > div:nth-child(2)')))
 	except:
 		msg = 'could not find options button'
 		driver.quit()
@@ -56,7 +72,7 @@ def gettranscript(videoid):
 		return msg
 
 	try:
-	    element = WebDriverWait(driver, waittime).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#items > ytd-menu-service-item-renderer:nth-child(2) > yt-formatted-string"))) #items > ytd-menu-service-item-renderer:nth-child(2) > yt-formatted-string
+		element = WebDriverWait(driver, waittime).until(EC.presence_of_element_located((By.XPATH, '//yt-formatted-string[text()="Mostrar transcrição"]')))
 	except:
 		msg = 'could not find transcript in options menu'
 		driver.quit()
@@ -70,13 +86,13 @@ def gettranscript(videoid):
 		return msg
 
 	try:
-	    element = WebDriverWait(driver, waittime).until(EC.presence_of_element_located((By.CSS_SELECTOR, "ytd-transcript-body-renderer.style-scope")))
+		element = WebDriverWait(driver, waittime).until(EC.presence_of_element_located((By.ID, "segments-container")))
 	except:
 		msg = 'could not find transcript text'
 		driver.quit()
 		return msg
 
-	#print(element.text)
+	# print(element.text)
 
 	file = open(writefilename,"w")
 	file.write(element.text)
@@ -86,16 +102,6 @@ def gettranscript(videoid):
 
 	return 'ok'
 
-# log function
-def logit(id,msg):
-	logwriter.writerow({'id':id,'msg':msg})
-	
-
-# prepare log file
-logwrite = open('captions.log','w',newline='\n')
-logwriter = csv.DictWriter(logwrite, fieldnames=['id','msg'])
-logwriter.writeheader()
-
 # read CSV file
 csvread = open(filename, newline='\n')
 csvreader = csv.DictReader(csvread, delimiter=delimiter, quoting=csv.QUOTE_NONE)
@@ -103,6 +109,6 @@ rowcount = len(open(filename).readlines())
 
 for row in csvreader:
 	msg = gettranscript(row[colname])
-	logit(row[colname],msg)
+	logging.info(f'{dt} | {str(rowcount)} | {row[colname]} | {msg}')
 	rowcount -= 1
-	print(str(rowcount) + " :  " + row[colname] + " : " + msg)
+	print(dt + " | " + str(rowcount) + " | " + row[colname] + " | " + msg)
